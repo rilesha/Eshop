d9f0e0e8fe6ad13e5f24e6a3830b4c7704c09248
usernameioioioio <rileshs2@gmail.com>
2026-08-04 07:17:09 +0545

    added more items

--- db.sql
+++ db.sql
@@ -37,3 +37,12 @@ INSERT INTO products (name, description, price, stock, image, created_at) VALUES
 ('Liverpool FC Home Jersey 2025', 'Authentic Liverpool 2025/26 home jersey. The famous red jersey.', 4200.00, 22, 'liverpool.png', '2026-07-24 20:53:57'),
 ('Juventus FC Home Jersey 2025', 'Official Juventus 2025/26 home jersey. Timeless black and white.', 4000.00, 15, 'juventus.png', '2026-07-24 20:53:57'),
 ('Paris Saint-Germain Home Jersey 2025', 'Authentic PSG 2025/26 home jersey. Sleek navy blue design.', 4300.00, 20, 'psg.png', '2026-07-24 20:53:57');
+
+-- Insert Football Boots
+INSERT INTO products (name, description, price, stock, image, created_at) VALUES
+('Adidas F50 Elite FG', 'Engineered for ultimate speed, lightweight Fibertouch upper with Sprintframe 360 outsole.', 28500.00, 15, 'adidas_f50.png', CURRENT_TIMESTAMP),
+('Nike Mercurial Vapor 16 Elite', 'Features a 3/4-length Air Zoom unit and Gripknit upper for incredible touch and explosive acceleration.', 31000.00, 12, 'nike_vapor16.png', CURRENT_TIMESTAMP),
+('Puma Future 7 Ultimate', 'FUZIONFIT360 upper with PWRTAPE for custom lock-in and dynamic movement control.', 24000.00, 18, 'puma_future.png', CURRENT_TIMESTAMP),
+('Adidas Predator 24 Elite FT', 'Iconic fold-over tongue with Strikeskin rubber fins for precision curve and shooting.', 29500.00, 10, 'adidas_predator24.png', CURRENT_TIMESTAMP),
+('Nike Phantom GX II Elite', 'Revolutionary Nike Gripknit texture for pin-point precision passing and shooting.', 29000.00, 14, 'nike_phantom_gx.png', CURRENT_TIMESTAMP),
+('Mizuno Morelia Neo IV Beta Japan', 'Handcrafted in Japan with premium K-leather for unmatched touch and lightweight feel.', 34000.00, 8, 'mizuno_morelia.png', CURRENT_TIMESTAMP);
